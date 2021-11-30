23580075ad4e1637cedc9b0ebcfdfc56f00cca66
sara-boughalemi <sarraboughalmi012@gmail.com>
2021-11-30 16:22:37 +0100

    my_Work

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import MainNavbar from "./components/MainNavbar/MainNavbar.js";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <MainNavbar />
+    </div>
+  );
+}
+
+export default App;
